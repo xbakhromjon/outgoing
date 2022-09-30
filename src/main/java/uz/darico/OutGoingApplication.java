6ae31eddd3df134c4a1b51328817064ae5c65bf6
xbakhromjon <xbakhromjon@gmail.com>
2022-09-30 17:22:55 +0500

    cors

--- src/main/java/uz/darico/OutGoingApplication.java
+++ src/main/java/uz/darico/OutGoingApplication.java
@@ -4,8 +4,16 @@ package uz.darico;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Arrays;
 
 
 @SpringBootApplication
@@ -15,5 +23,15 @@ public class OutGoingApplication {
     public static void main(String[] args) {
 
         SpringApplication.run(OutGoingApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowCredentials(true).allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "PATCH").allowedHeaders("*").allowedOrigins("http://localhost:3000");
+            }
+        };
     }
 }
