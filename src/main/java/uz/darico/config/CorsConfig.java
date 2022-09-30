10fa97421450154f3b10fb26f9358cfd054c0acf
xbakhromjon <xbakhromjon@gmail.com>
2022-09-30 16:34:43 +0500

    cors

---
+++ src/main/java/uz/darico/config/CorsConfig.java
@@ -0,0 +1,32 @@
+package uz.darico.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
+/**
+ * @author : Bakhromjon Khasanboyev
+ * @username: @xbakhromjon
+ * @since : 30/09/22, Fri, 16:33
+ **/
+@Configuration
+public class CorsConfig {
+    @Configuration
+    public static class CorsConfiguration extends org.springframework.web.cors.CorsConfiguration {
+        @Bean
+        public CorsWebFilter corsFilter() {
+            org.springframework.web.cors.CorsConfiguration corsConfiguration = new org.springframework.web.cors.CorsConfiguration();
+            corsConfiguration.setAllowCredentials(true);
+            corsConfiguration.addAllowedOrigin("http://localhost:3000/");
+//            corsConfiguration.addAllowedOrigin("http://www.new.d-doc.uz/");
+            corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "PATCH"));
+            corsConfiguration.addAllowedHeader("*");
+            UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+            source.registerCorsConfiguration("/**", corsConfiguration);
+            return new CorsWebFilter(source);
+        }
+    }
+}
