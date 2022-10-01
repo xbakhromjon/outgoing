8124bd99543772dca204952cf6942459e02f8088
xbakhromjon <xbakhromjon@gmail.com>
2022-10-01 13:04:43 +0500

    test

--- src/main/java/uz/darico/test/Test2Controller.java
+++ src/main/java/uz/darico/test/Test2Controller.java
@@ -1,9 +1,20 @@
 package uz.darico.test;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import uz.darico.base.controller.AbstractController;
 
+@RestController
+@RequestMapping("/test")
 public class Test2Controller extends AbstractController<Test2Service> {
     public Test2Controller(Test2Service service) {
         super(service);
+    }
+
+    @GetMapping
+    public ResponseEntity<?> test() {
+        return ResponseEntity.ok("Hello");
     }
 }
